f85c196812e98d3b8da92c1e521fd566be0aa862
LEESEUNGBUM <shonori710@gmail.com>
2025-11-10 14:45:53 +0900

    3-08

--- src/main/java/com/mysite/sbb/question/Question.java
+++ src/main/java/com/mysite/sbb/question/Question.java
@@ -1,19 +1,20 @@
 package com.mysite.sbb.question; 
 
-import java.time.LocalDateTime; 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import com.mysite.sbb.answer.Answer;
+import com.mysite.sbb.user.SiteUser;
 
-import jakarta.persistence.CascadeType; 
-import jakarta.persistence.Column; 
-import jakarta.persistence.Entity; 
-import jakarta.persistence.GeneratedValue; 
-import jakarta.persistence.GenerationType; 
-import jakarta.persistence.Id; 
-import jakarta.persistence.OneToMany; 
-
-import lombok.Getter; 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
 import lombok.Setter; 
 
 @Getter 
@@ -34,4 +35,7 @@ public class Question {
 
     @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE) 
     private List<Answer> answerList; 
+    
+    @ManyToOne
+    private SiteUser author;
 }
